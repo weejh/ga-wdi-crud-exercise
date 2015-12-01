bc27f696690b3c0ef3f2b1ec7081ba360e19aab3
Sebastiaan Deckers <seb@ninja.sg>
2015-12-01 17:18:59 +0800

    Mongoose to update records in the database

--- app/app.js
+++ app/app.js
@@ -3,27 +3,18 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
 const app = express()
-const filepath = path.join(__dirname, '../data/scoreboard.json')
-let scoreboard
 
-app.use((req, res, next) => {
-  fs.readFile(filepath, 'utf8', (err, data) => {
-    if (err) console.warn('Failed to read file')
-    else scoreboard = JSON.parse(data)
-    if (req.method !== 'GET') {
-      const fin = res.end
-      res.end = function () {
-        fs.writeFileSync(
-          filepath,
-          JSON.stringify(scoreboard, null, '  ')
-        )
-        return fin.apply(this, arguments)
-      }
-    }
-    next()
-  })
+const dbUri = 'mongodb://' +
+  process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD +
+  '@ds061984.mongolab.com:61984/playground'
+mongoose.connect(dbUri)
+
+var Score = mongoose.model('Score', {
+  player: String,
+  score: Number
 })
 
 app.use(express.static('public'))
@@ -40,19 +31,48 @@ app.get('/scores/:id', (req, res) => {
 
 // create
 app.post('/scores', (req, res) => {
-  const score = req.body
-  scoreboard.push(score)
-  res.json(score)
+  const score = new Score(req.body)
+  score.save(err => {
+    if (err) return console.error(err)
+    res.json(req.body)
+    console.log('score saved!')
+  })
 })
 
 // update
-app.put('/scores/:id', (req, res) => {
-  scoreboard[req.params.id] = req.body
-  res.json(scoreboard[req.params.id])
+app.put('/scores/:player', (req, res) => {
+  const query = { player: req.params.player }
+  // Callback style:
+  Score.findOneAndUpdate(query, req.body, { new: true }, (err, score) => {
+    if (err) {
+      console.error(err)
+      res.status(500).end(err)
+    } else if (score) {
+      console.log('found and updated', JSON.stringify(score))
+      res.json(score)
+    } else {
+      console.log('not found')
+      res.status(404).end('Score Not Found')
+    }
+  // Promise style:
+  // Score.findOneAndUpdate(query, req.body, { new: true })
+  //   .then(score => {
+  //     if (score) {
+  //       console.log('found and updated', JSON.stringify(score))
+  //       res.json(score)
+  //     } else {
+  //       console.log('not found')
+  //       res.status(404).end('Score Not Found')
+  //     }
+  //   })
+  //   .catch(err => {
+  //     console.error(err)
+  //     res.status(500).end(err)
+  //   })
 })
 
 // delete
-app.delete('/scores/:id', (req, res) => {
+app.delete('/scores/:name', (req, res) => {
   delete scoreboard[req.params.id]
   res.send(scoreboard)
 })
